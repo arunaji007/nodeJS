4f7ad01609195fc6647d55be4ee9d02702b29ff0
arunkumar.duraisamy <arunkumar.duraisamy@umm.digital>
2024-10-17 13:38:36 +0530

    updated database

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+const {Pool}  = require('pg');
 const express = require('express');
 const app = express();
 const port = 3000;
+const pgPool = new Pool({
+  connectionString: process.env.STP_MY_DATABASE_CONNECTION_STRING // env variable was automatically injected by Stacktape
+});
+
+app.get('/time', async (req, res) => {
+  const result = await pgPool.query('SELECT NOW()');
+  const time = result.rows[0];
+
+  res.send(time);
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
